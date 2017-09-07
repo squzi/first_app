aea9473a1060a3ddebcc187271465f6a2099073b
squzi <chuck_79@mail.ru>
2017-09-08 02:15:22 +0300

    Initial repository

---
+++ Documents/Aptana Studio 3 Workspace/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'bdc58a88283111343706ddcf3586bb709082d8238a55b69d8469e3903d0221a769059df47cc0c33798629bddfc3b11147a5f4d05ccad686e2e85b3f52a6b8e14'
